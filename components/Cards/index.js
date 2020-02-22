c5db0ebb8ffa389bc2f7ec9609dd79edb1ad1ef2
Mohamed-Essamaali <mohamed.essamaali@gmail.com>
2020-02-21 21:58:30 -0500

    Header and Tab are completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res=>{
+    const cardsContainer = document.querySelector('.cards-container');
+
+    cardsContainer.appendChild( cardCreator(res.data,'bootstrap'));
+})
+.catch(err=>{
+  console.log('failed here is the error',err)
+});
+
+const cardCreator = (obj,topic)=>{
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = `headline ${obj.articles.topic}`;
+    card.appendChild(headline);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    card.appendChild(author);
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    author.appendChild(imgContainer);
+
+    const authorImage = document.createElement('img');
+    authorImage.src = `${obj.articles.topic}`;
+    imgContainer.appendChild(authorImage);
+    const name = document.createElement('span');
+    name.textContent = `${obj.articles.topic}`;
+    author.appendChild(name);
+    
+    return card;
+
+}
